57b6d548528979642582b639ff4ad4ea85aaf444
Ваше имя пользователя на GitHub <skvip8492@gmail.com>
2026-03-27 18:14:31 +0300

    commit

--- src/main/java/service/ShoppingCart.java
+++ src/main/java/service/ShoppingCart.java
@@ -18,7 +18,6 @@ public class ShoppingCart {
         return sumProducts;
     }
 
-
     public double getShoppingCartAmountDiscount() {
         double sumProducts = 0;
         for (int i = 0; i < shoppingCartArray.length; i++) {
